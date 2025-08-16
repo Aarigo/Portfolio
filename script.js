3a2607dcce422b9725c5d66efe0b1c4594d1d378
Aarigo <166893581+Aarigo@users.noreply.github.com>
2025-08-16 12:00:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+#script.js
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -53,4 +54,5 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
+
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
